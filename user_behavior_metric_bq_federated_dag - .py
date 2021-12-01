f5fd10f5e7587a502a0fe7a76f3ef859ada495c3
OscarGarciaF <garcia.oscar1729@gmail.com>
2021-11-30 23:45:11 -0600

    add where to drop nulls

--- user_behavior_metric_bq_federated_dag - .py
+++ user_behavior_metric_bq_federated_dag - .py
@@ -35,9 +35,9 @@ insert_into_table_query = (
         f"TRUNCATE TABLE {DATASET_NAME}.{TABLE_NAME}; "
         f"INSERT INTO {DATASET_NAME}.{TABLE_NAME} "
         f"SELECT * FROM EXTERNAL_QUERY(\"projects/deliverable3-oscargarciaf/locations/us-central1/connections/deliverable3-postgres-conn\", "
-        f"\"\"\"WITH review_analytics AS (SELECT cid, SUM(positive_review) AS review_score , COUNT(cid) AS review_count  FROM silver.reviews GROUP BY cid ), "
-        f"user_analytics AS (SELECT customer_id, CAST(SUM(quantity * unit_price) AS DECIMAL(18, 5)) AS amount_spent FROM bronze.user_purchase GROUP BY customer_id ) "
-        f"SELECT COALESCE(cid, customer_id) AS customer_id, COALESCE(amount_spent, 0) AS amount_spent, COALESCE(review_score, 0) AS review_score, COALESCE(review_count, 0) AS review_count, CURRENT_DATE AS insert_date "
+        f"\"\"\"WITH review_analytics AS (SELECT cid, SUM(positive_review) AS review_score , COUNT(cid) AS review_count  FROM silver.reviews GROUP BY cid WHERE cid IS NOT NULL), "
+        f"user_analytics AS (SELECT customer_id, CAST(SUM(quantity * unit_price) AS DECIMAL(18, 5)) AS amount_spent FROM bronze.user_purchase GROUP BY customer_id WHERE customer_id IS NOT NULL) "
+        f"SELECT COALESCE(ra.cid, ua.customer_id) AS customer_id, COALESCE(amount_spent, 0) AS amount_spent, COALESCE(review_score, 0) AS review_score, COALESCE(review_count, 0) AS review_count, CURRENT_DATE AS insert_date "
         f"FROM review_analytics ra "
         f"FULL JOIN user_analytics ua ON ra.cid = ua.customer_id;\"\"\"); "
     )
