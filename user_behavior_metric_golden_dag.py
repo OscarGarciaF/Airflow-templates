6943a46feb16ac9882b9fae5644f5d850a357186
OscarGarciaF <garcia.oscar1729@gmail.com>
2021-11-30 23:52:34 -0600

    update old dag

--- user_behavior_metric_golden_dag.py
+++ user_behavior_metric_golden_dag.py
@@ -40,8 +40,10 @@ create_table_query = f"""CREATE TABLE IF NOT EXISTS {SCHEMA_NAME}.{TABLE_NAME} (
                             insert_date DATE);"""
                             
 create_insert_into_table = f"""WITH review_analytics AS (SELECT cid, SUM(positive_review) AS review_score , COUNT(cid) AS review_count  FROM silver.reviews
+                            WHERE cid IS NOT NULL
                             GROUP BY cid ),
                             user_analytics AS (SELECT customer_id, CAST(SUM(quantity * unit_price) AS DECIMAL(18, 5)) AS amount_spent FROM bronze.user_purchase
+                            WHERE customer_id IS NOT NULL
                             GROUP BY customer_id )
                             SELECT COALESCE(ua.customer_id, ra.cid) AS customer_id, COALESCE(amount_spent, 0) AS amount_spent, COALESCE(review_score, 0) AS review_score, COALESCE(review_count, 0) AS review_count, CURRENT_DATE AS insert_date                      
                             FROM review_analytics ra
