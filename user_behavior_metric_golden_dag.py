af39295d587f42252796580ad98c91b6002c6f68
OscarGarciaF <garcia.oscar1729@gmail.com>
2021-11-30 23:40:09 -0600

    change order of coalesce

--- user_behavior_metric_golden_dag.py
+++ user_behavior_metric_golden_dag.py
@@ -43,7 +43,7 @@ create_insert_into_table = f"""WITH review_analytics AS (SELECT cid, SUM(positiv
                             GROUP BY cid ),
                             user_analytics AS (SELECT customer_id, CAST(SUM(quantity * unit_price) AS DECIMAL(18, 5)) AS amount_spent FROM bronze.user_purchase
                             GROUP BY customer_id )
-                            SELECT COALESCE(cid, customer_id) AS customer_id, COALESCE(amount_spent, 0) AS amount_spent, COALESCE(review_score, 0) AS review_score, COALESCE(review_count, 0) AS review_count, CURRENT_DATE AS insert_date                      
+                            SELECT COALESCE(ua.customer_id, ra.cid) AS customer_id, COALESCE(amount_spent, 0) AS amount_spent, COALESCE(review_score, 0) AS review_score, COALESCE(review_count, 0) AS review_count, CURRENT_DATE AS insert_date                      
                             FROM review_analytics ra
                             FULL JOIN user_analytics ua ON ra.cid = ua.customer_id;"""
                         
